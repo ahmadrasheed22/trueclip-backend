b864a1f9ba2da20d9ee41e66d001faacde95a43a
ahmadrasheed22 <ahmad22rasheed22@gmail.com>
2026-04-22 21:29:50 +0500

    Increase Express body size limits to 100mb

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use('/clips', express.static('/tmp/clips'));
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
