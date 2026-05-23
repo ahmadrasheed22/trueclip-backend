4d5a341a4f480faf0e3108173da26cc2a570103e
ahmadrasheed22 <ahmad22rasheed22@gmail.com>
2026-05-23 20:58:30 +0500

    Configure CORS for Vercel origin

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const CLIPS_DIR = '/tmp/clips';
 const clipIndex = new Map();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: 'https://trueclip.vercel.app' }));
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use('/clips', express.static(CLIPS_DIR));
